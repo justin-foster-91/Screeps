954e492d331bfa098f1477853b88a403c6c4e4ed
Justin Foster <jfoster91@gmail.com>
2020-12-15 16:46:36 -0700

    Working of creep list object

--- main.js
+++ main.js
@@ -1,80 +1,91 @@
-var roleHarvester = require('role.harvester');
-var roleUpgrader = require('role.upgrader');
-var roleBuilder = require('role.builder');
+var roleHarvester = require("role.harvester");
+var roleUpgrader = require("role.upgrader");
+var roleBuilder = require("role.builder");
 
 module.exports.loop = function () {
-    // How to build containers
-    // How to harvest multiple sources
+  // How to build containers
+  // How to harvest multiple sources
 
   // If memory contains a creep that doesn't exist anymore, delete it from memory.
   // Why is this necessary?
-  for(var name in Memory.creeps) {
-    if(!Game.creeps[name]) {
-        delete Memory.creeps[name];
-        console.log('Clearing non-existing creep memory:', name);
+  for (var name in Memory.creeps) {
+    if (!Game.creeps[name]) {
+      delete Memory.creeps[name];
+      console.log("Clearing non-existing creep memory:", name);
     }
   }
+
+  // const creepSpawnOrder = () => {
+
+  // }
 
   // Isolate and define creep with the role of Harvester
   // What is _.filter ?
   const spawnCreeps = (role, spawner) => {
-      let units = _.filter(Game.creeps, (creep) => creep.memory.role == `${role}`);
-      if (units.length < 4) {
-        // Possibly slice off spawner number. Requires naming convention
-        let newName = `${role.charAt(0).toUpperCase() + role.slice(1)}`+ spawner + Game.time;
-        console.log("Spawning new harvester: " + newName);
-        Game.spawns[spawner].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
-          memory: { role: `${role}` },
-        });
-      }
+    let units = _.filter(
+      Game.creeps,
+      (creep) => creep.memory.role == `${role}`
+    );
+    if (units.length < 4) {
+      // Possibly slice off spawner number. Requires naming convention
+      let newName =
+        `${role.charAt(0).toUpperCase() + role.slice(1)}` + spawner + Game.time;
+      console.log("Spawning new harvester: " + newName);
+      Game.spawns[spawner].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
+        memory: { role: `${role}` },
+      });
     }
+  };
 
-    spawnCreeps('harvester', 'Spawn1');
-    spawnCreeps('upgrader', 'Spawn1');
-    spawnCreeps('builder', 'Spawn1');
+  spawnCreeps("harvester", "Spawn1");
+  spawnCreeps("upgrader", "Spawn1");
+  spawnCreeps("builder", "Spawn1");
 
   // If Spawn1 is making a creep, show the role being spawned
-  if(Game.spawns['Spawn1'].spawning) { 
-      // Assigns the creep name to spawningCreep variable
-      var spawningCreep = Game.creeps[Game.spawns['Spawn1'].spawning.name];
-      // Displays working icon and the role of the creep being spawned
-      Game.spawns['Spawn1'].room.visual.text(
-          '🛠️' + spawningCreep.memory.role,
-          Game.spawns['Spawn1'].pos.x + 1, 
-          Game.spawns['Spawn1'].pos.y, 
-          {align: 'left', opacity: 0.8});
+  if (Game.spawns["Spawn1"].spawning) {
+    // Assigns the creep name to spawningCreep variable
+    var spawningCreep = Game.creeps[Game.spawns["Spawn1"].spawning.name];
+    // Displays working icon and the role of the creep being spawned
+    Game.spawns["Spawn1"].room.visual.text(
+      "🛠️" + spawningCreep.memory.role,
+      Game.spawns["Spawn1"].pos.x + 1,
+      Game.spawns["Spawn1"].pos.y,
+      { align: "left", opacity: 0.8 }
+    );
   }
 
-  
-  var tower = Game.getObjectById('97525daefefc04fa54f53707');
+  var tower = Game.getObjectById("97525daefefc04fa54f53707");
   // If tower exists...
-  if(tower) {
-      // If there is a damaged structure, use the tower to repair it
-      var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
-          filter: (structure) => structure.hits < structure.hitsMax
-      });
-      if(closestDamagedStructure) {
-          tower.repair(closestDamagedStructure);
+  if (tower) {
+    // If there is a damaged structure, use the tower to repair it
+    var closestDamagedStructure = tower.pos.findClosestByRange(
+      FIND_STRUCTURES,
+      {
+        filter: (structure) => structure.hits < structure.hitsMax,
       }
+    );
+    if (closestDamagedStructure) {
+      tower.repair(closestDamagedStructure);
+    }
 
-      // If there is a hostile, attack it
-      var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
-      if(closestHostile) {
-          tower.attack(closestHostile);
-      }
+    // If there is a hostile, attack it
+    var closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (closestHostile) {
+      tower.attack(closestHostile);
+    }
   }
 
   // If harvester, upgrader, or builder exist, then run their code
-  for(var name in Game.creeps) {
-      var creep = Game.creeps[name];
-      if(creep.memory.role == 'harvester') {
-          roleHarvester.run(creep);
-      }
-      if(creep.memory.role == 'upgrader') {
-          roleUpgrader.run(creep);
-      }
-      if(creep.memory.role == 'builder') {
-          roleBuilder.run(creep);
-      }
+  for (var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    if (creep.memory.role == "harvester") {
+      roleHarvester.run(creep);
+    }
+    if (creep.memory.role == "upgrader") {
+      roleUpgrader.run(creep);
+    }
+    if (creep.memory.role == "builder") {
+      roleBuilder.run(creep);
+    }
   }
-}
+};
--- scrapPad2.js
+++ scrapPad2.js
@@ -1,9 +1,41 @@
-// role.harvester.js
+let creepRoleList = {
+  harvester: {
+    name: ,
+    spawner: ,
+    quantity: ,
+    parts: ,
+    priority: ,
+  },
+  upgrader: {
 
-// Get creep current position
-let check = creeps.pos()
-// Check if position has a structure
-if (Game.rooms.sim.lookForAt(LOOK_CONSTRUCTION_SITES, check).length === 0) {
-  // If no structure, place road construction site
-  Game.spawns['Spawn1'].room.createConstructionSite(check, STRUCTURE_ROAD );
-} 
+  },
+  builder: {
+
+  },
+}
+
+const creepSpawnOrder = () => {
+
+}
+
+// Isolate and define creep with the role of Harvester
+// What is _.filter ?
+const spawnCreeps = (role, spawner) => {
+  let units = _.filter(
+    Game.creeps,
+    (creep) => creep.memory.role == `${role}`
+  );
+  if (units.length < 4) {
+    // Possibly slice off spawner number. Requires naming convention
+    let newName =
+      `${role.charAt(0).toUpperCase() + role.slice(1)}` + spawner + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    Game.spawns[spawner].spawnCreep([WORK, CARRY, MOVE, MOVE], newName, {
+      memory: { role: `${role}` },
+    });
+  }
+};
+
+spawnCreeps("harvester", "Spawn1");
+spawnCreeps("upgrader", "Spawn1");
+spawnCreeps("builder", "Spawn1");
